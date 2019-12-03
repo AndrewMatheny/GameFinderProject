544fa63854f59f99292b527069f5bdfa11939487
AndrewMatheny <github email address>
2019-12-03 14:54:38 -0800

    Added basic review controller/new view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,36 @@
 class ReviewsController < ApplicationController
+    def show
+        find_review
+    end
+
+    def new
+        @review = Review.new
+    end
+
+    def create
+        @review = Review.new(review_params)
+        @review.save
+        redirect_to @review
+    end
+
+    def edit
+        find_review
+    end
+
+    def update
+        @review = Review.update(review_params)
+        @review.save
+        redirect_to @review
+    end
+
+    private
+
+    def review_params
+        params.require(:review).permit(:score, :text, :user_id, :game_id)
+    end
+
+    def find_review
+        @review = Review.find(params[:id])
+    end
+
 end
