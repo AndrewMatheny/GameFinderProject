657a0ff68a07ef3681bbc80546774fdc128b1859
AndrewMatheny <github email address>
2019-12-03 14:11:37 -0800

    setup basic user page+create

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+    def show
+        find_user
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        @user.save
+        redirect_to @user
+    end
+
+    def edit
+        find_user
+    end
+
+    def update
+        @user = User.update(user_params)
+        redirect_to @user
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:name, :age, :bio)
+    end
+
+    def find_user
+        @user = User.find(params[:id])
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  resources :game_genres
+  resources :genres
+  resources :reviews
+  resources :games
+  resources :users, only: [:show, :new, :create]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
