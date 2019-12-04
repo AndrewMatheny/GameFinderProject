5157b467dfc8a70417b28ecdc8b855708eeedc36
AndrewMatheny <github email address>
2019-12-04 15:16:07 -0800

    Takes in genres and associates in join table for game

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,17 +5,28 @@ class GamesController < ApplicationController
 
     def new
         @game = Game.new
+        @gamegenre = GameGenre.new(game_id: 1, genre_id: 1)
     end
 
     def create
         @game = Game.new(game_params)
         @game.save
+        # ids = params[:game][:genre_ids]
+        # #sanitize ids before next step
+        # clean_ids = ids.reject { |e| e.to_s.empty? }
+        # clean_ids.each do |id|
+        #     GameGenre.create(game_id: @game.id, genre_id: id)
+        # end
+        
+        #@gamegenre.save
+
+        #byebug
         redirect_to @game
     end
 
     private
 
     def game_params
-        params.require(:game).permit(:name, :developer, :release_date)
+        params.require(:game).permit(:name, :developer, :release_date, genre_ids: [])
     end
 end
