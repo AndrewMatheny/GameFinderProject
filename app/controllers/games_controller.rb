265c29d1a03df5144871c480f86de634dc50eeea
AndrewMatheny <github email address>
2019-12-04 14:11:31 -0800

    add new game but genre not accepting

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,4 +2,20 @@ class GamesController < ApplicationController
     def show
         @game = Game.find(params[:id])
     end
+
+    def new
+        @game = Game.new
+    end
+
+    def create
+        @game = Game.new(game_params)
+        @game.save
+        redirect_to @game
+    end
+
+    private
+
+    def game_params
+        params.require(:game).permit(:name, :developer, :release_date)
+    end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -3,4 +3,5 @@ class Game < ApplicationRecord
     has_many :users, through: :reviews
     has_many :game_genres
     has_many :genres, through: :game_genres
+    #accepts_nested_attributes_for :genres
 end
