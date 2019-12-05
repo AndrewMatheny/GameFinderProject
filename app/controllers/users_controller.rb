21d657924b538d3b3de13a6b9dfc1d0146fac9e3
AndrewMatheny <github email address>
2019-12-05 10:30:10 -0800

    fixed bug in updating profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,9 @@ class UsersController < ApplicationController
     end
 
     def update
-        @user = User.update(user_params)
-        redirect_to @user
+        @user = User.find(params[:id])
+        @user.update(user_params)
+        redirect_to user_path
     end
 
     def destroy
